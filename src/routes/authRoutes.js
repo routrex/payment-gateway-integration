2b62c8de43039a315efcf4d45e50a148456f9df4
routrex <rifaldiekaputra637@gmail.com>
2026-09-03 23:12:52 +0700

    adds routing and function middleware for profile

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import {
+  getProfile,
   googleOAuth,
   googleOAuthCallback,
 } from "../controller/authController.js";
+import verifyToken from "../middleware/verifyToken.js";
 
 const route = express.Router();
 
-// route.get("/profile", verifyToken, getProfile)
+route.get("/profile", verifyToken, getProfile)
 route.get("/google", googleOAuth);
 route.get("/google/callback", googleOAuthCallback);
 // route.post("/logout", logout);
